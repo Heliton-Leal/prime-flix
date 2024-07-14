aa40e3d69891aab607d89bf51f2b2ebd5c8ce380
Heliton-Leal <sleal.heliton@outlook.com>
2024-07-14 16:12:53 -0300

    Alterado arquivo da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// FULL URL: https://api.themoviedb.org/3/movie/now_playing?api_key=70ad69b0526aacbfb76afd9f9fc70f40
+// FULL URL: https://api.themoviedb.org/3/movie/now_playing?api_key=70ad69b0526aacbfb76afd9f9fc70f
 // Base URL: https://api.themoviedb.org/3/
-// Chave API: 70ad69b0526aacbfb76afd9f9fc70f40
+// Chave API: 70ad69b0526aacbfb76afd9f9fc70f
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
