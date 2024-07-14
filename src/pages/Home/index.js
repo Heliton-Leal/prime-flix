3dd44124b3560451d0e5558ded6aef1ec3de7853
Heliton-Leal <sleal.heliton@outlook.com>
2024-07-14 16:07:45 -0300

    Criado serviço de conexão com API externa

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,39 @@
-function Home() {
+import { useEffect, useState } from "react";
+import api from '../../services/api';
+import { Link } from "react-router-dom";
+import './home.css';
+// https://api.themoviedb.org/3/movie/now_playing?api_key=70ad69b0526aacbfb76afd9f9fc70f40
+
+function Home(){
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=>{
+    async function loadFilmes(){
+      const resposnse = await api.get('movie/now_playing', {
+        params: {
+          api_key: '70ad69b0526aacbfb76afd9f9fc70f40',
+          language: 'pt-BR',
+          page: 1,
+        }
+      })
+      setFilmes(resposnse.data.results.slice(0, 10));
+    }
+    loadFilmes()
+  }, []);
+
   return(
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme)=> {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   )
 }
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// FULL URL: https://api.themoviedb.org/3/movie/now_playing?api_key=70ad69b0526aacbfb76afd9f9fc70f40
+// Base URL: https://api.themoviedb.org/3/
+// Chave API: 70ad69b0526aacbfb76afd9f9fc70f40
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+});
+
+export default api;
